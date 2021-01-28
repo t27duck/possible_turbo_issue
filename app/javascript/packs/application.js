43af5ce16a299a3a4c0bdb6ddd3878ef780ffedf
Tony Drake <tony.drake@highalphastudio.com>
2021-01-28 14:09:03 -0500

    add turbo, remove turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,9 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import { Turbo, cable } from "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
